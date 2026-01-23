a85303687f92163e6db074a87f2faca6e664c841
Paulo Costa <eu@paulo.costa.nom.br>
2026-01-23 17:23:54 -0300

    First commit

---
+++ test/slashid-agent.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SlashidAgent from '../lib/slashid-agent-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/slashid-agent-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SlashidAgent.SlashidAgentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
